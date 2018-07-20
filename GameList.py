0f6497007df0c391845c2cccff7a3c5afc1443d9
yuzusiro <kitsunegadget@gmail.com>
2018-07-20 11:58:48 +0900

    パス，ゲームエンド処理追加．ゲームになった．

--- GameList.py
+++ GameList.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tkinter as tk
+import tkinter.messagebox as tkMsg
 
 class ReverseGame:
     """リバーシゲーム
@@ -47,14 +48,14 @@ class ReverseGame:
     def Start(self):
         """ゲームスタート
         """
-        print("Reverse Start!")
+        print("Reversi Start!")
         self.root.title("リバーシ: 黒の番です")
         return self.board
 
     def __StatusUpdate(self):
         """状態をGUIに反映します
         """
-        print("update!")
+        #print("update!")
         for i, l1 in enumerate(self.board):
             for j, val in enumerate(l1):
                 if val == -2:
@@ -78,7 +79,12 @@ class ReverseGame:
         j = int(text[1])
         self.board[i][j] = int(self.__playerorder)
         self.__putCalc(order=self.__playerorder, y=i, x=j, reverse=True)
-
+        self.__playerChange()
+    
+    def __playerChange(self, passCount=0):
+        """プレイヤーの交代\n
+        passCount: ゲーム内連続パスカウント、2連続したらお互いに置ける場所がないのでゲーム終了
+        """
         self.__playerorder = not self.__playerorder
         self.__searchCalc(self.__playerorder)
         self.__StatusUpdate()
@@ -95,7 +101,40 @@ class ReverseGame:
             player = "白"
             strings = f"リバーシ: {player}の番です　黒: {count_black}　白: {count_white}"
             self.root.title(strings)
-    
+        
+        if np.sum(self.board == -2) == 0 and np.sum(self.board == -1) == 0:
+            self.__endGame(black=count_black, white=count_white)
+            return
+        
+        elif np.sum(self.board == -1) == 0:
+            if passCount == 1:
+                self.__endGame(black=count_black, white=count_white)
+                return
+
+            tkMsg.showinfo(f"{player} Pass", "置ける場所がないためパスします", parent=self.root)
+            self.__playerChange(passCount=1)
+        
+        
+
+    def __endGame(self, black, white):
+        """ゲーム終了処理
+        """
+        if black > white:
+            winner = "黒"
+        elif white > black:
+            winner = "白"
+        else:
+            tkMsg.showinfo("ゲーム終了", "同点です", parent=self.root)
+            strings = f"リバーシ: 同点です　黒: {black}　白: {white}"
+            self.root.title(strings)
+            print("Game End.")
+            return
+
+        tkMsg.showinfo("ゲーム終了", f"{winner} の勝ちです", parent=self.root)
+        strings = f"リバーシ: {winner} の勝ちです　黒: {black}　白: {white}"
+        self.root.title(strings)
+        print("Game End.")
+        
     def __searchCalc(self, playerorder):
         """現在のプレイヤーのコマの置ける位置を探索します
         """
@@ -117,7 +156,7 @@ class ReverseGame:
                                     self.__putCalc(playerorder, i+h, j+w)
         
     def __putCalc(self, order, y, x, reverse=False):
-        """その場所からコマが置けるか探索
+        """その場所(y,x)にコマが置けるか探索\n
         reverse: コマの入れ替えをするときにTrue
         """
         for h in range(-1, 2):
